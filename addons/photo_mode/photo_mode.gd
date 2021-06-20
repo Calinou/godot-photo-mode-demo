fcd17f85fea8a940b451fd64035c66aba9c81d4a
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-06-20 19:02:17 +0200

    Temporarily set high-quality graphics settings during the screenshot

--- addons/photo_mode/photo_mode.gd
+++ addons/photo_mode/photo_mode.gd
@@ -3,6 +3,15 @@ extends Control
 ## Path to the folder where screenshots are saved.
 const SCREENSHOTS_DIR = "user://screenshots"
 
+# Graphics settings to be restored at the end of taking a screenshot.
+var old_msaa := int(ProjectSettings.get_setting("rendering/anti_aliasing/quality/msaa"))
+var old_shadow_quality := int(ProjectSettings.get_setting("rendering/shadows/directional_shadow/soft_shadow_quality"))
+var old_shadow_size := int(ProjectSettings.get_setting("rendering/shadows/directional_shadow/size"))
+var old_shadow_16_bits := bool(ProjectSettings.get_setting("rendering/shadows/directional_shadow/16_bits"))
+# TODO: Requires RenderingServer SSAO quality methods to be exposed.
+var old_ssao_quality := int(ProjectSettings.get_setting("rendering/environment/ssao/quality"))
+var old_ssao_adaptive_target := float(ProjectSettings.get_setting("rendering/environment/ssao/adaptive_target"))
+
 
 func _ready() -> void:
 	# Create folder as soon as possible so that it can be opened using the
@@ -12,6 +21,9 @@ func _ready() -> void:
 
 
 func _input(event: InputEvent) -> void:
+	# TODO: Implement shortcuts.
+	return
+	
 	if event.is_action_pressed("toggle_photo_mode"):
 		visible = not visible
 		
@@ -25,13 +37,32 @@ func _input(event: InputEvent) -> void:
 		await take_screenshot(true)
 
 
+func apply_high_quality_settings() -> void:
+	RenderingServer.directional_shadow_quality_set(RenderingServer.SHADOW_QUALITY_SOFT_ULTRA)
+	RenderingServer.directional_shadow_atlas_set_size(8192, false)
+	# 16× MSAA causes various issues and may freeze the system entirely.
+	# Stick to 8× MSAA which looks nearly as good.
+	get_viewport().msaa = Viewport.MSAA_8X
+
+
+func restore_old_quality_settings() -> void:
+	RenderingServer.directional_shadow_quality_set(old_shadow_quality)
+	RenderingServer.directional_shadow_atlas_set_size(old_shadow_size, old_shadow_16_bits)
+	get_viewport().msaa = old_msaa
+
+
 func take_screenshot(high_quality: bool = true) -> void:
 	var viewport := get_viewport()
 	
 	# Perform super-sample antialiasing by rendering at a higher resolution, then downscaling the final image.
 	# Unlike MSAA, this smooths out things such as specular aliasing.
-	const SUPERSAMPLE_FACTOR = 2
+	# FIXME: Window size changes when increasing viewport size, which looks strange
+	# and doesn't work in fullscreen.
+	const SUPERSAMPLE_FACTOR = 1
 	viewport.size *= SUPERSAMPLE_FACTOR
+	
+	if high_quality:
+		apply_high_quality_settings()
 
 	# Wait some frames to get an up-to-date screenshot.
 	await get_tree().process_frame
@@ -41,6 +72,9 @@ func take_screenshot(high_quality: bool = true) -> void:
 	var image: Image = viewport.get_texture().get_image()
 	image.resize(image.get_width() / SUPERSAMPLE_FACTOR, image.get_height() / SUPERSAMPLE_FACTOR, Image.INTERPOLATE_CUBIC)
 	#viewport.render_target_clear_mode = SubViewport.CLEAR_MODE_ALWAYS
+	
+	if high_quality:
+		restore_old_quality_settings()
 	
 	viewport.size /= SUPERSAMPLE_FACTOR
 
