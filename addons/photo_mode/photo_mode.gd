536cd7cf373ea18b957b0fb33618fff13e7e32cc
Hugo Locurcio <hugo.locurcio@hugo.pro>
2023-02-21 23:40:20 +0100

    Update for 4.0.rc3

--- addons/photo_mode/photo_mode.gd
+++ addons/photo_mode/photo_mode.gd
@@ -12,21 +12,20 @@ const SUPERSAMPLE_FACTOR = 2.0
 
 # Graphics settings to be restored at the end of taking a screenshot.
 
-var old_msaa := int(ProjectSettings.get_setting("rendering/anti_aliasing/quality/msaa"))
+var old_msaa := int(ProjectSettings.get_setting("rendering/anti_aliasing/quality/msaa_3d"))
 var old_screen_space_aa := int(ProjectSettings.get_setting("rendering/anti_aliasing/quality/screen_space_aa"))
 var old_3d_scale := float(ProjectSettings.get_setting("rendering/scaling_3d/scale"))
 var old_debanding := bool(ProjectSettings.get_setting("rendering/anti_aliasing/quality/use_debanding"))
 
-var old_directional_shadow_quality := int(ProjectSettings.get_setting("rendering/shadows/directional_shadow/soft_shadow_quality"))
-var old_directional_shadow_size := int(ProjectSettings.get_setting("rendering/shadows/directional_shadow/size"))
-var old_directional_shadow_16_bits := bool(ProjectSettings.get_setting("rendering/shadows/directional_shadow/16_bits"))
+var old_directional_shadow_quality := int(ProjectSettings.get_setting("rendering/lights_and_shadows/directional_shadow/soft_shadow_filter_quality"))
+var old_directional_shadow_size := int(ProjectSettings.get_setting("rendering/lights_and_shadows/directional_shadow/size"))
+var old_directional_shadow_16_bits := bool(ProjectSettings.get_setting("rendering/lights_and_shadows/directional_shadow/16_bits"))
 
-var old_point_shadow_quality := int(ProjectSettings.get_setting("rendering/shadows/shadows/soft_shadow_quality"))
-var old_point_shadow_size := int(ProjectSettings.get_setting("rendering/shadows/shadow_atlas/size"))
-var old_point_shadow_16_bits := bool(ProjectSettings.get_setting("rendering/shadows/shadow_atlas/16_bits"))
+var old_positional_shadow_quality := int(ProjectSettings.get_setting("rendering/lights_and_shadows/positional_shadow/soft_shadow_filter_quality"))
+var old_positional_shadow_size := int(ProjectSettings.get_setting("rendering/lights_and_shadows/positional_shadow/atlas_size"))
+var old_positional_shadow_16_bits := bool(ProjectSettings.get_setting("rendering/lights_and_shadows/positional_shadow/atlas_16_bits"))
 
 var old_glow_upscale_mode := int(ProjectSettings.get_setting("rendering/environment/glow/upscale_mode"))
-var old_glow_high_quality := bool(ProjectSettings.get_setting("rendering/environment/glow/use_high_quality"))
 @onready var old_env_glow_level_1: float = environment.get("glow_levels/1")
 @onready var old_env_glow_level_2: float = environment.get("glow_levels/2")
 @onready var old_env_glow_level_3: float = environment.get("glow_levels/3")
@@ -42,7 +41,7 @@ var old_dof_jitter = bool(ProjectSettings.get_setting("rendering/camera/depth_of
 var old_subsurface_scattering_quality := int(ProjectSettings.get_setting("rendering/environment/subsurface_scattering/subsurface_scattering_quality"))
 
 var old_ssr_quality := int(ProjectSettings.get_setting("rendering/environment/screen_space_reflection/roughness_quality"))
-@onready var old_env_ssr_max_steps: int = environment.ss_reflections_max_steps
+@onready var old_env_ssr_max_steps: int = environment.ssr_max_steps
 
 var old_ssao_quality := int(ProjectSettings.get_setting("rendering/environment/ssao/quality"))
 var old_ssao_half_size := bool(ProjectSettings.get_setting("rendering/environment/ssao/half_size"))
@@ -71,7 +70,7 @@ var old_sdfgi_probe_ray_count := int(ProjectSettings.get_setting("rendering/glob
 var old_sdfgi_frames_to_converge := int(ProjectSettings.get_setting("rendering/global_illumination/sdfgi/frames_to_converge"))
 var old_sdfgi_frames_to_update_lights := int(ProjectSettings.get_setting("rendering/global_illumination/sdfgi/frames_to_update_lights"))
 @onready var old_env_sdfgi_occlusion: bool = environment.sdfgi_use_occlusion
-@onready var old_env_sdfgi_num_cascades: RenderingServer.EnvironmentSDFGICascades = environment.sdfgi_cascades
+@onready var old_env_sdfgi_num_cascades: int = environment.sdfgi_cascades
 @onready var old_env_sdfgi_min_cell_size: float = environment.sdfgi_min_cell_size
 
 # TODO: High-quality settings for environment sky filtering. Requires setters to be exposed in RenderingServer
@@ -79,16 +78,15 @@ var old_sdfgi_frames_to_update_lights := int(ProjectSettings.get_setting("render
 
 
 func _ready() -> void:
-	if get_viewport().get_camera_3d().effects == null:
+	if get_viewport().get_camera_3d().attributes == null:
 		# Required to set depth of field properties in the GUI later on.
-		# FIXME: This doesn't work; setting DoF properties appears to have no effect if no CameraEffects
+		# FIXME: This doesn't work; setting DoF properties appears to have no effect if no CameraAttributes
 		# resource is present (i.e. if this code branch runs).
-		get_viewport().get_camera_3d().effects = CameraEffects.new()
+		get_viewport().get_camera_3d().attributes = CameraAttributesPractical.new()
 
 	# Create folder as soon as possible so that it can be opened using the
 	# Open Screenshots Folder button.
-	var dir := Directory.new()
-	dir.make_dir_recursive(SCREENSHOTS_DIR)
+	DirAccess.make_dir_recursive_absolute(SCREENSHOTS_DIR)
 
 
 func _input(event: InputEvent) -> void:
@@ -118,7 +116,7 @@ func apply_high_quality_sdfgi_settings() -> void:
 	RenderingServer.environment_set_sdfgi_frames_to_update_light(RenderingServer.ENV_SDFGI_UPDATE_LIGHT_IN_1_FRAME)
 	# Disable SDFGI occlusion as it can introduce dark artifacts and is unnecessary with a low cell size.
 	environment.sdfgi_use_occlusion = false
-	environment.sdfgi_cascades = Environment.SDFGI_CASCADES_8
+	environment.sdfgi_cascades = 8
 	# Higher number of cascades makes it possible to decrease the cell size without compromising on maximum distance.
 	# This improves GI quality for smaller objects.
 	environment.sdfgi_min_cell_size *= 0.25
@@ -126,20 +124,18 @@ func apply_high_quality_sdfgi_settings() -> void:
 
 ## Increase rendering quality settings for offline rendering.
 func apply_high_quality_settings() -> void:
-	# Use high-resolution 24-bit shadows.
-	# 16K shadows are technically supported, but tend to require too much memory when supersampling.
-	# Therefore, 8K shadows are used instead.
-	RenderingServer.directional_shadow_quality_set(RenderingServer.SHADOW_QUALITY_SOFT_ULTRA)
-	RenderingServer.directional_shadow_atlas_set_size(8192, false)
-	RenderingServer.shadows_quality_set(RenderingServer.SHADOW_QUALITY_SOFT_ULTRA)
-	get_viewport().shadow_atlas_size = 8192
-	get_viewport().shadow_atlas_16_bits = false
+	# Use high-resolution 16-bit shadows.
+	# 24-bit shadows are technically supported for higher precision, but tend to require too much memory when supersampling.
+	RenderingServer.directional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_ULTRA)
+	RenderingServer.directional_shadow_atlas_set_size(16384, true)
+	RenderingServer.positional_soft_shadow_filter_set_quality(RenderingServer.SHADOW_QUALITY_SOFT_ULTRA)
+	get_viewport().positional_shadow_atlas_size = 16384
+	get_viewport().positional_shadow_atlas_16_bits = true
 
 	RenderingServer.voxel_gi_set_quality(RenderingServer.VOXEL_GI_QUALITY_HIGH)
 
 	# Increase post-processing effects quality.
 	RenderingServer.environment_glow_set_use_bicubic_upscale(true)
-	RenderingServer.environment_glow_set_use_high_quality(true)
 	# Glow blurriness is resolution-dependent, so adjust the levels to compensate (use blurrier levels).
 	# This won't match when using glow levels different from the defaults, but most projects stick
 	# to the defaults anyway.
@@ -151,23 +147,23 @@ func apply_high_quality_settings() -> void:
 		environment.set("glow_levels/5", 0.0)
 		environment.set("glow_levels/6", 1.0)
 		environment.set("glow_levels/7", 0.0)
-	RenderingServer.camera_effects_set_dof_blur_bokeh_shape(RenderingServer.DOF_BOKEH_CIRCLE)
-	RenderingServer.camera_effects_set_dof_blur_quality(RenderingServer.DOF_BLUR_QUALITY_HIGH, old_dof_jitter)
+	RenderingServer.camera_attributes_set_dof_blur_bokeh_shape(RenderingServer.DOF_BOKEH_CIRCLE)
+	RenderingServer.camera_attributes_set_dof_blur_quality(RenderingServer.DOF_BLUR_QUALITY_HIGH, old_dof_jitter)
 	# Depth of field amount is resolution-dependent, so adjust the amount to compensate.
-	get_viewport().get_camera_3d().effects.dof_blur_amount *= SUPERSAMPLE_FACTOR
+	get_viewport().get_camera_3d().attributes.dof_blur_amount *= SUPERSAMPLE_FACTOR
 
 	RenderingServer.sub_surface_scattering_set_quality(RenderingServer.SUB_SURFACE_SCATTERING_QUALITY_HIGH)
 	# Keep the existing SSAO and SSIL fadeout values as they can be used for artistic control.
 	RenderingServer.environment_set_ssao_quality(RenderingServer.ENV_SSAO_QUALITY_ULTRA, false, 1.0, 6, old_ssao_fadeout_from, old_ssao_fadeout_to)
-	RenderingServer.environment_set_ssil_quality(RenderingServer.ENV_SSAO_QUALITY_ULTRA, false, 1.0, 6, old_ssil_fadeout_from, old_ssil_fadeout_to)
-	RenderingServer.environment_set_ssr_roughness_quality(RenderingServer.ENV_SSR_ROUGNESS_QUALITY_HIGH)
+	RenderingServer.environment_set_ssil_quality(RenderingServer.ENV_SSIL_QUALITY_ULTRA, false, 1.0, 6, old_ssil_fadeout_from, old_ssil_fadeout_to)
+	RenderingServer.environment_set_ssr_roughness_quality(RenderingServer.ENV_SSR_ROUGHNESS_QUALITY_HIGH)
 	# Screen-space reflections' length is resolution-dependent, so adjust the number of steps to compensate.
-	environment.ss_reflections_max_steps *= SUPERSAMPLE_FACTOR
+	environment.ssr_max_steps *= SUPERSAMPLE_FACTOR
 
 	RenderingServer.environment_set_volumetric_fog_volume_size(512, 512)
 	RenderingServer.environment_set_volumetric_fog_filter_active(true)
 
-	get_viewport().msaa = Viewport.MSAA_8X
+	get_viewport().msaa_3d = Viewport.MSAA_8X
 	# Since the rendering is supersampled, we don't lose much sharpness (if any)
 	# by enabling FXAA. FXAA will still help further reduce shader-induced aliasing
 	# even when supersampling.
@@ -178,17 +174,16 @@ func apply_high_quality_settings() -> void:
 ## Restore original quality settings (both SDFGI and other settings).
 func restore_old_quality_settings() -> void:
 	# Restore original shadow quality.
-	RenderingServer.directional_shadow_quality_set(old_directional_shadow_quality)
+	RenderingServer.directional_soft_shadow_filter_set_quality(old_directional_shadow_quality)
 	RenderingServer.directional_shadow_atlas_set_size(old_directional_shadow_size, old_directional_shadow_16_bits)
-	RenderingServer.shadows_quality_set(old_point_shadow_quality)
-	get_viewport().shadow_atlas_size = old_point_shadow_size
-	get_viewport().shadow_atlas_16_bits = old_point_shadow_16_bits
+	RenderingServer.positional_soft_shadow_filter_set_quality(old_positional_shadow_quality)
+	get_viewport().positional_shadow_atlas_size = old_positional_shadow_size
+	get_viewport().positional_shadow_atlas_16_bits = old_positional_shadow_16_bits
 
 	RenderingServer.voxel_gi_set_quality(old_voxel_gi_quality)
 
-	# Restore original post-processing effects quality.
+	# Restore original post-processing attributes quality.
 	RenderingServer.environment_glow_set_use_bicubic_upscale(old_glow_upscale_mode)
-	RenderingServer.environment_glow_set_use_high_quality(old_glow_high_quality)
 	environment.set("glow_levels/1", old_env_glow_level_1)
 	environment.set("glow_levels/2", old_env_glow_level_2)
 	environment.set("glow_levels/3", old_env_glow_level_3)
@@ -196,9 +191,9 @@ func restore_old_quality_settings() -> void:
 	environment.set("glow_levels/5", old_env_glow_level_5)
 	environment.set("glow_levels/6", old_env_glow_level_6)
 	environment.set("glow_levels/7", old_env_glow_level_7)
-	RenderingServer.camera_effects_set_dof_blur_bokeh_shape(old_dof_bokeh_shape)
-	RenderingServer.camera_effects_set_dof_blur_quality(old_dof_quality, old_dof_jitter)
-	get_viewport().get_camera_3d().effects.dof_blur_amount /= SUPERSAMPLE_FACTOR
+	RenderingServer.camera_attributes_set_dof_blur_bokeh_shape(old_dof_bokeh_shape)
+	RenderingServer.camera_attributes_set_dof_blur_quality(old_dof_quality, old_dof_jitter)
+	get_viewport().get_camera_3d().attributes.dof_blur_amount /= SUPERSAMPLE_FACTOR
 	RenderingServer.sub_surface_scattering_set_quality(old_subsurface_scattering_quality)
 	RenderingServer.environment_set_ssao_quality(
 			old_ssao_quality,
@@ -217,7 +212,7 @@ func restore_old_quality_settings() -> void:
 			old_ssil_fadeout_to
 	)
 	RenderingServer.environment_set_ssr_roughness_quality(old_ssr_quality)
-	environment.ss_reflections_max_steps = old_env_ssr_max_steps
+	environment.ssr_max_steps = old_env_ssr_max_steps
 
 	RenderingServer.environment_set_volumetric_fog_volume_size(old_volumetric_fog_volume_size, old_volumetric_fog_volume_depth)
 	RenderingServer.environment_set_volumetric_fog_filter_active(old_volumetric_fog_filter)
@@ -230,7 +225,7 @@ func restore_old_quality_settings() -> void:
 	environment.sdfgi_cascades = old_env_sdfgi_num_cascades
 	environment.sdfgi_min_cell_size = old_env_sdfgi_min_cell_size
 
-	get_viewport().msaa = old_msaa
+	get_viewport().msaa_3d = old_msaa
 	get_viewport().screen_space_aa = old_screen_space_aa
 	get_viewport().use_debanding = old_debanding
 
@@ -299,35 +294,35 @@ func take_screenshot(high_quality: bool = true) -> void:
 
 
 func _on_dof_near_enabled_toggled(button_pressed: bool) -> void:
-	get_viewport().get_camera_3d().effects.dof_blur_near_enabled = button_pressed
+	get_viewport().get_camera_3d().attributes.dof_blur_near_enabled = button_pressed
 
 
 func _on_dof_near_distance_value_changed(value: float) -> void:
-	get_viewport().get_camera_3d().effects.dof_blur_near_distance = value
+	get_viewport().get_camera_3d().attributes.dof_blur_near_distance = value
 	$Panel/VBoxContainer/DOFNearDistance/Value.text = str(value)
 
 
 func _on_dof_near_transition_value_changed(value: float) -> void:
-	get_viewport().get_camera_3d().effects.dof_blur_near_transition = value
+	get_viewport().get_camera_3d().attributes.dof_blur_near_transition = value
 	$Panel/VBoxContainer/DOFNearTransition/Value.text = str(value)
 
 
 func _on_dof_far_enabled_toggled(button_pressed: bool) -> void:
-	get_viewport().get_camera_3d().effects.dof_blur_far_enabled = button_pressed
+	get_viewport().get_camera_3d().attributes.dof_blur_far_enabled = button_pressed
 
 
 func _on_dof_far_distance_value_changed(value: float) -> void:
-	get_viewport().get_camera_3d().effects.dof_blur_far_distance = value
+	get_viewport().get_camera_3d().attributes.dof_blur_far_distance = value
 	$Panel/VBoxContainer/DOFFarDistance/Value.text = str(value)
 
 
 func _on_dof_far_transition_value_changed(value: float) -> void:
-	get_viewport().get_camera_3d().effects.dof_blur_far_transition = value
+	get_viewport().get_camera_3d().attributes.dof_blur_far_transition = value
 	$Panel/VBoxContainer/DOFFarTransition/Value.text = str(value)
 
 
 func _on_dof_amount_value_changed(value: float) -> void:
-	get_viewport().get_camera_3d().effects.dof_blur_amount = value
+	get_viewport().get_camera_3d().attributes.dof_blur_amount = value
 	$Panel/VBoxContainer/DOFAmount/Value.text = str(value)
 
 
@@ -350,12 +345,12 @@ func _on_adjustments_saturation_value_changed(value: float) -> void:
 
 
 func _on_reset_to_defaults_pressed() -> void:
-	$Panel/VBoxContainer/DOFNearEnabled.pressed = false
+	$Panel/VBoxContainer/DOFNearEnabled.button_pressed = false
 	$Panel/VBoxContainer/DOFNearDistance/HSlider.value = 5.0
 	$Panel/VBoxContainer/DOFNearTransition/HSlider.value = 5.0
 	$Panel/VBoxContainer/DOFFarDistance/HSlider.value = 30.0
 	$Panel/VBoxContainer/DOFFarTransition/HSlider.value = 15.0
-	$Panel/VBoxContainer/DOFFarEnabled.pressed = false
+	$Panel/VBoxContainer/DOFFarEnabled.button_pressed = false
 	$Panel/VBoxContainer/DOFAmount/HSlider.value = 0.1
 	$Panel/VBoxContainer/AdjustmentsBrightness/HSlider.value = 1.0
 	$Panel/VBoxContainer/AdjustmentsContrast/HSlider.value = 1.0
